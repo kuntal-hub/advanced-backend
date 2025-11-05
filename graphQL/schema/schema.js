76de3dfbfb79094d71e655be7099bfb4434edf57
Kuntal Maity <142421401+kuntal-hub@users.noreply.github.com>
2025-11-05 17:19:21 +0000

    implement user input and mutation for adding and updating users

--- graphQL/schema/schema.js
+++ graphQL/schema/schema.js
@@ -1,4 +1,13 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } from "graphql";
+import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList, GraphQLInputObjectType, GraphQLNonNull } from "graphql";
+
+const users = [
+    { id: 1, name: 'Alice', email: 'alice@example.com', age: 25 },
+    { id: 2, name: 'Bob', email: 'bob@example.com', age: 30 },
+    { id: 3, name: 'Charlie', email: 'charlie@example.com', age: 35 },
+    { id: 4, name: 'David', email: 'david@example.com', age: 40 }
+];
+
+// we must make <object>Types by using GraphQLObjectType any external types (like mongoose schema models) do not work
 
 const userType = new GraphQLObjectType({
     name: 'User',
@@ -10,6 +19,17 @@ const userType = new GraphQLObjectType({
     }
 });
 
+const userInputType = new GraphQLInputObjectType({
+    name: 'userInput',
+    fields: {
+        name:{type:new GraphQLNonNull(GraphQLString)},
+        email:{type:new GraphQLNonNull(GraphQLString)},
+        age:{type:GraphQLInt},
+    }
+})
+
+
+// Define the Root Query
 const rootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -25,20 +45,68 @@ const rootQuery = new GraphQLObjectType({
             type: userType,
             args: { id: { type: GraphQLInt } },
             resolve: (parent, args) => {
-                const users = [
-                    { id: 1, name: 'Alice', email: 'alice@example.com', age: 25 },
-                    { id: 2, name: 'Bob', email: 'bob@example.com', age: 30 },
-                    { id: 3, name: 'Charlie', email: 'charlie@example.com', age: 35 },
-                    { id: 4, name: 'David', email: 'david@example.com', age: 40 }
-                ];
                 return users.find(user => user.id === args.id);
+            }
+        },
+        users: {
+            type: new GraphQLList(userType),
+            resolve: function resolve(parent, args) {
+                return users;
             }
         }
     }
 });
 
+const rootMutation = new GraphQLObjectType({
+    name:'userMutaion',
+    fields:{
+        addUser: {
+            type: userType,
+            args: {
+                input:{type:userInputType}
+            },
+            resolve(parent,{input}) {
+                if(!input.age) {
+                    throw new Error('provide age ')
+                }
+
+                const newUser = {
+                    name: input.name,
+                    email: input.email,
+                    age: input.age,
+                    id: users.length + 1
+                }
+
+                users.push(newUser);
+
+                return newUser;
+            }
+        },
+        updateUser: {
+            type: userType,
+            args: {
+                id: { type: GraphQLInt },
+                name: { type: GraphQLString },
+                email: { type: GraphQLString },
+                age: { type: GraphQLInt }
+            },
+            resolve(parent,args) {
+                    users[args.id -1] = {
+                    name: args.name,
+                    email: args.email,
+                    age: args.age,
+                    id: args.id
+                }
+
+                return users[args.id -1];
+            }
+        },
+    }
+})
+
 const schema = new GraphQLSchema({
-    query: rootQuery
+    query: rootQuery,
+    mutation: rootMutation
 });
 
 export default schema;
